f2717be6ec4d5a367d95e005993b2c885222896b
tttsymq <525300595@qq.com>
2021-05-23 02:03:10 +0800

    111

---
+++ ZLLib/Classes/CHButtonSimple.h
@@ -0,0 +1,16 @@
+//
+//  CHButtonSimple.h
+//  ZLLib
+//
+//  Created by wch on 2021/5/23.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHButtonSimple : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
