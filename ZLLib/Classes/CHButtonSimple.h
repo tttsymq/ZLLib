a5f4defdc780b7bcead3f0306ef485799ca29f94
tttsymq <525300595@qq.com>
2021-05-23 02:27:02 +0800

    test

--- ZLLib/Classes/CHButtonSimple.h
+++ ZLLib/Classes/CHButtonSimple.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CHButtonSimple : UIButton
 
+- (void)test;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- ZLLib/Classes/CHButtonSimple.m
+++ ZLLib/Classes/CHButtonSimple.m
@@ -9,12 +9,9 @@
 
 @implementation CHButtonSimple
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)test
+{
+    
 }
-*/
 
 @end
